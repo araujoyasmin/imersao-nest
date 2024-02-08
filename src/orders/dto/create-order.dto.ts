d77dc1f6217a0d9aaebcf753166f7202b00e644f
araujoyasmin <yasaraujo@icloud.com>
2024-02-08 00:55:27 -0300

    :sparkles: auth and rabbitmq

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,14 +1,27 @@
-export class CreateOrderDto {
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsInt, IsNotEmpty, IsPositive, IsString, MaxLength, ValidateNested } from "class-validator";
 
+export class CreateOrderDto {
+    @ArrayNotEmpty()
+    @ValidateNested({each:true})
+    @Type(() => OrdemItemDto)
     items: OrdemItemDto[];
 
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
     card_hash: string;
 
 }
 
 
 export class OrdemItemDto{
+    @IsNotEmpty()
+    @IsInt()
+    @IsPositive()
     quantity: number;
 
+    @MaxLength(36)
+    @IsString()
     product_id: string;
 }
